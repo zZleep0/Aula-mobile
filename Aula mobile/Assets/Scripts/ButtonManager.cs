5273a74c1e714b5702fa0a2d45573d3f89dffe0f
zZleep0 <152449401+zZleep0@users.noreply.github.com>
2024-09-09 16:37:04 -0300

    tentativa de fazer a atividade

--- Aula mobile/Assets/Scripts/ButtonManager.cs
+++ Aula mobile/Assets/Scripts/ButtonManager.cs
@@ -5,6 +5,22 @@ using UnityEngine.SceneManagement;
 
 public class ButtonManager : MonoBehaviour
 {
+    public GameObject pauseMenu;
+    public GridManager gridManager;
+
+    private void Start()
+    {
+        if (pauseMenu == null)
+        {
+            pauseMenu = GameObject.Find("Pause Menu");
+        }
+        pauseMenu.SetActive(false);
+    }
+    public void Update()
+    {
+        Pause();
+    }
+
     public void Iniciar()
     {
         SceneManager.LoadScene("Fase 1");
@@ -18,5 +34,23 @@ public class ButtonManager : MonoBehaviour
     public void VoltarParaMenu()
     {
         SceneManager.LoadScene("Tela Inicial");
+    }
+
+    public void Pause()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            pauseMenu.SetActive(true);
+        }
+        
+    }
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+    }
+
+    public void BuffScore()
+    {
+        
     }
 }
